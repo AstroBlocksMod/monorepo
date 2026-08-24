cf6b79723132e9f6a41b25ac68ff58cdecfac15e
playforge-coding <251060555+playforge-coding@users.noreply.github.com>
2026-08-24 17:08:31 -0500

    port server from ob

--- packages/scratch-vm/src/engine/runtime.js
+++ packages/scratch-vm/src/engine/runtime.js
@@ -485,6 +485,22 @@ class Runtime extends EventEmitter {
         this.isPackaged = false;
 
         /**
+         * astro: We support a "privileged" mode. This usually is set when the project is running as a server,
+         * but other privileged clients can use this too.
+         * This is mainly to indicate that system APIs (possibly mocked and/or with a permission system)
+         * can be accessed, as provided by the privileged client.
+         */
+        this.isPrivileged = false;
+
+        /**
+         * astro: Privileged utilities, so that the VM can communicate with a privileged client.
+         * This is usually filled in by the server client, but another client can fill this in too,
+         * as long as they are compatible and set isPrivileged to true.
+         * @type {Object<string, function>}
+         */
+        this.privilegedUtils = Object.create(null);
+
+        /**
          * Contains information about the external communication methods that the scripts inside the project
          * can use to send data from inside the project to an external server.
          * Do not update this directly. Use Runtime.setExternalCommunicationMethod() instead.
@@ -926,6 +942,20 @@ class Runtime extends EventEmitter {
      */
     static get PLATFORM_MISMATCH () {
         return 'PLATFORM_MISMATCH';
+    }
+
+    /**
+     * astro: Event name when a web request is forwarded to the VM.
+     */
+    static get SERVER_REQUEST () {
+        return 'SERVER_REQUEST';
+    }
+
+    /**
+     * astro: Event name when a response to a web request is forwarded to the web request handler.
+     */
+    static get SERVER_RESPONSE () {
+        return 'SERVER_RESPONSE';
     }
 
     /**
--- packages/scratch-vm/src/engine/thread.js
+++ packages/scratch-vm/src/engine/thread.js
@@ -216,6 +216,28 @@ class Thread {
         this.procedures = null;
         this.executableHat = false;
         this.compatibilityStackFrame = null;
+
+        /**
+         * astro: The object the web server stores a request in.
+         * @type {object}
+         */
+        this.serverRequest = {
+            ip: '',
+            method: '',
+            page: '',
+            headers: '{}',
+            data: ''
+        };
+
+        /**
+         * astro: The object the web server constructs the response in.
+         * @type {object}
+         */
+        this.serverResponse = {
+            mime: 'text/plain',
+            status: null, // Initialized by the request listener hat.
+            headers: '{}'
+        };
     }
 
     /**
